fb3dbb23ef2d3488e700f2a81d0d1c0bf96e0eea
Maurice Le Cordier <maurice.lecordier@SAMAC141.local>
2018-02-05 18:09:53 +0200

    starting fresh

--- scripts.js
+++ scripts.js
@@ -4,10 +4,18 @@ Use straight javascript and avoid JQuery. You can use any resource to set this u
 Some hints are given below but you can use any structure or approach of your choosing.
 
 */
-
-var button = document.querySelector('.targetClassName');
-
-button.addEventListener('click', function(e) {
-
-  
+function toggleVisibility() {
+    let block = document.getElementById("block1");
+    if (block.style.display === "none") {
+        block.style.display = "block";
+    } else {
+        block.style.display = "none";
+    }
+}
+ 
+let hideContentButton = document.getElementById('hideContentButton');
+hideContentButton.addEventListener('click', function()
+{
+    console.log("clicked");
+    toggleVisibility();
 });
